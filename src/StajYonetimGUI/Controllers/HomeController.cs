da60864b7a2a0241fb8d11095da600327441195f
busecansuuu <busecanu316@gmail.com>
2024-01-08 06:43:00 +0300

    user sayfası eklendi

--- src/StajYonetimGUI/Controllers/HomeController.cs
+++ src/StajYonetimGUI/Controllers/HomeController.cs
@@ -23,7 +23,11 @@ namespace StajYonetimGUI.Controllers
         {
             return View();
         }
-       
+        public IActionResult UserPage()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
